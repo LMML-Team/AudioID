71dab3473f41699d9a8c7889d4cf9ea12da9de6d
Anonymission <marrs2k@gmail.com>
2017-07-14 09:44:08 -0400

    added function to record audio input and return audio samples array

--- audio_identification.py
+++ audio_identification.py
@@ -7,6 +7,8 @@ from scipy.ndimage.filters import maximum_filter
 from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
 from scipy.ndimage.morphology import iterate_structure
 
+from microphone import record_audio
+from microphone import play_audio
 
 song_data = {}
 
@@ -14,7 +16,7 @@ song_data = {}
 def new_song(song_path, sf=44100) :
     '''Adds new song to database with song_name as key
     and tuple of samples, sampling rate,
-    (fingerprint dictionary -- to be added) and song_path as value.
+    (fingerprint dictionary -- to be implemented) and song_path as value.
 
     Parameters
     -------------
@@ -24,8 +26,25 @@ def new_song(song_path, sf=44100) :
         sampling frequency of song (default = 44100)
     '''
     song_name = song_path[-song_path[::-1].find("/") :
-                          -song_path[::-1].find(".") - 1] if "/" in song_path
-                          else song_pathsong_path[: -song_path[::-1].find(".") - 1]
+                          -song_path[::-1].find(".") - 1] if "/" in song_path else song_pathsong_path[: -song_path[::-1].find(".") - 1]
     if song_name not in song_data :
         samples, sf = librosa.load(song_path, sr=sf)
         song_data[song_name] = (samples, sf, song_path)
+
+
+def record_song(time=10) :
+    '''Records song and converts samples from bits to python integers
+
+    Parameters
+    -------------
+    time: int
+        length of time to record audio (default = 10)
+
+    Return
+    -------------
+    samples: np.array, dtype = np.int16
+        array of audio samples as integers
+    '''
+    byte_encoded_signal, sr = record_audio(time)
+    samples = np.hstack(tuple(np.fromstring(i, dtype=np.int16) for i in byte_encoded_signal))
+    return samples
