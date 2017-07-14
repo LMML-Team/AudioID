5295b58d067ea9254c95fb3293c4500eac68e891
impostercafe <mlai8088@gmail.com>
2017-07-14 15:05:14 -0400

    Added spectrogram

--- audio_identification/transformation.py
+++ audio_identification/transformation.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
 def convert_frequencies(samples):
     '''Given samples from a song, this function will convert
     the samples into an array of frequencies
@@ -15,4 +19,9 @@ def convert_frequencies(samples):
     k = np.arange(len(coefs)) / length  # Frequencies
     return [coefs, length, k]
 
-def make_spectrogram()
+def print_spectrogram(samples, fs=44100):
+    S, freqs, times = mlab.specgram(samples, NFFT=4096, Fs=fs,
+                                    window=mlab.window_hanning,
+                                    noverlap=(4096 // 2))
+    return np.array(S, freqs, times)
+
