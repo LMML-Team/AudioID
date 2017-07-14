a5b51d8d52a7c174877d5934996b9f0509dd7e21
CandyMandy28 <amanda.wang294@gmail.com>
2017-07-14 09:48:31 -0400

    the start of the thingy stuffs. idk, it's a start.

---
+++ database.py
@@ -0,0 +1,44 @@
+
+
+
+def add_song(song):
+    """
+    Adds specified song into the dictionary of songs
+    --------------
+    Parameters:
+    song
+    
+    --------------
+    Returns:
+    
+    
+    """
+    pass
+
+def remove_song(song):
+    """
+    Removes specified song from the dictionary of songs
+    --------------
+    Parameters:
+    
+    
+    --------------
+    Returns:
+    
+    
+    """
+    pass
+
+def list_songs():
+    """
+    returns the list of songs as a np.array
+    --------------
+    Parameters:
+    N/A
+    
+    --------------
+    Returns:
+    
+    
+    """
+    return 
