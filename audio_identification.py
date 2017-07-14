d63a8ef86f6a0e0ffd634a428ca074891acc35c1
Anonymission <marrs2k@gmail.com>
2017-07-14 10:27:31 -0400

    moved functions from audio_identification to database

--- audio_identification.py
+++ audio_identification.py
@@ -10,41 +10,6 @@ from scipy.ndimage.morphology import iterate_structure
 from microphone import record_audio
 from microphone import play_audio
 
-song_data = {}
-song_text = open('workfile', 'rb+')
-
-def save():
-    '''Saves song_data to a .txt file and 
-
-    Parameters
-    -------------
-    N/A
-
-    Return
-    -------------
-    N/A
-    '''
-    
-
-def new_song(song_path, sf=44100) :
-    '''Adds new song to database with song_name as key
-    and tuple of samples, sampling rate,
-    (fingerprint dictionary -- to be implemented) and song_path as value.
-
-    Parameters
-    -------------
-    song_path: r"PATH"
-        string with song's path
-    sf: int > 0
-        sampling frequency of song (default = 44100)
-    '''
-    song_name = song_path[-song_path[::-1].find("/") :
-                          -song_path[::-1].find(".") - 1] if "/" in song_path else song_pathsong_path[: -song_path[::-1].find(".") - 1]
-    if song_name not in song_data :
-        samples, sf = librosa.load(song_path, sr=sf)
-        song_data[song_name] = (samples, sf, song_path)
-
-
 def record_song(time=10) :
     '''Records song and converts samples from bits to python integers
 
@@ -61,8 +26,3 @@ def record_song(time=10) :
     byte_encoded_signal, sr = record_audio(time)
     samples = np.hstack(tuple(np.fromstring(i, dtype=np.int16) for i in byte_encoded_signal))
     return samples
-
-
-def list_songs():
-    """Returns the list of song names as a np.array"""
-    return song_data.keys()
