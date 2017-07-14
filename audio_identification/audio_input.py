5dc458848ad9a01349ce07c8b31e6fc0e4d12a1c
Anonymission <marrs2k@gmail.com>
2017-07-14 16:19:11 -0400

    added audio_input.py file

---
+++ audio_identification/audio_input.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import librosa
+
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
+from scipy.ndimage.morphology import iterate_structure
+
+from microphone import record_audio
+from microphone import play_audio
+
+def record_song(time=10) :
+    '''Records song and converts samples from bits to python integers
+
+    Parameters
+    -------------
+    time: int
+        length of time to record audio (default = 10)
+
+    Return
+    -------------
+    samples: np.array, dtype = np.int16
+        array of audio samples as integers
+    '''
+    byte_encoded_signal, sr = record_audio(time)
+    samples = np.hstack(tuple(np.fromstring(i, dtype=np.int16) for i in byte_encoded_signal))
+    return samples
+
+
+def import_file(song_path, sf=44100) :
+    '''
+    Adds new song to database with song_name as key
+    and tuple of samples, sampling rate,
+    (fingerprint dictionary -- to be implemented) and song_path as value.
+
+    Parameters
+    -------------
+    song_path: r"PATH"
+        string with song's path
+    sf: int > 0
+        sampling frequency of song (default = 44100)
+
+    Return
+    -------------
+    sample: np.array
+        array of song samples
+    '''
+    song_name = song_path[-song_path[::-1].find("/") :
+                          -song_path[::-1].find(".") - 1] if "/" in song_path else song_path[: -song_path[::-1].find(".") - 1]
+    if song_name not in song_data :
+        samples, sf = librosa.load(song_path, sr=sf)
+        song_data[song_name] = (samples, sf)
+
+    return samples
