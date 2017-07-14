e75054a0948ac5159fe22500ec09c7b0cee4360a
impostercafe <mlai8088@gmail.com>
2017-07-14 15:58:07 -0400

    Renamed transformation.py

--- audio_identification/spectrogram.py
+++ audio_identification/spectrogram.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.mlab as mlab
 
-def print_spectrogram(samples, fs=44100):
+def make_spectrogram(samples, fs=44100):
+    '''
+    Takes the samples from
+
+    '''
     S, freqs, times = mlab.specgram(samples, NFFT=4096, Fs=fs,
                                     window=mlab.window_hanning,
                                     noverlap=(4096 // 2))
