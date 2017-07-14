24ad386bd03af33c9fd32b8cf717bca8d13816e1
CandyMandy28 <amanda.wang294@gmail.com>
2017-07-14 11:01:31 -0400

    add file reader/writer and put in code for some of the functions

--- database.py
+++ database.py
@@ -1,44 +1,65 @@
+import os
+
+# song_read = open('database.txt', 'r')
 
 
+def new_song(song_path, sf=44100) :
+    '''
+    Saves song to a .txt file
+    Adds new song to database with song_name as key
+    and tuple of samples, sampling rate,
+    (fingerprint dictionary -- to be implemented) and song_path as value.
 
-def add_song(song):
-    """
-    Adds specified song into the dictionary of songs
+    Parameters
+    -------------
+    song_path: r"PATH"
+        string with song's path
+    sf: int > 0
+        sampling frequency of song (default = 44100)
+    Returns
     --------------
-    Parameters:
-    song
+    N/A
+    '''
+    song_write = open('database.txt', 'w')
+    song_name = song_path[-song_path[::-1].find("/") :
+                          -song_path[::-1].find(".") - 1] if "/" in song_path else song_pathsong_path[: -song_path[::-1].find(".") - 1]
+    string ="\""+ song_name + "\" ::: " # + function of the finger printing
+    song_write.append(string)
     
-    --------------
-    Returns:
+    song_write.close()
     
-    
-    """
-    pass
+    # the data for a song before fourier transformation
+    song_data = {}
+    if song_name not in song_data :
+        samples, sf = librosa.load(song_path, sr=sf)
+        song_data[song_name] = (samples, sf, song_path)
 
-def remove_song(song):
+def remove_song(song_name):
     """
-    Removes specified song from the dictionary of songs
+    Removes specified song from 
+    
+    Parameters
     --------------
-    Parameters:
+    song_name: the name of the song
     
-    
+    Returns
     --------------
-    Returns:
-    
-    
+    N/A
     """
-    pass
+    song_write = open('database.txt', 'w')
+    song_close()
 
 def list_songs():
     """
-    returns the list of songs as a np.array
+    Returns the list of songs as a np.array
+    
+    Parameters
     --------------
-    Parameters:
     N/A
     
+    Returns
     --------------
-    Returns:
-    
-    
+    np.array: the list of song names
     """
-    return 
+    song_names = []
+    return song_names
