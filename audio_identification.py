b31731fce07467b9302d19b0c654fed60f83cd3e
Anonymission <marrs2k@gmail.com>
2017-07-13 20:21:19 -0400

    created function to add new song to python dictionary

---
+++ audio_identification.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import librosa
+
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
+from scipy.ndimage.morphology import iterate_structure
+
+
+song_data = {}
+
+
+def new_song(song_path, sf=44100) :
+    '''Adds new song to database with song_name as key
+    and tuple of samples, sampling rate, and song_path as value.
+
+    Parameters
+    -------------
+    song_path: r"PATH"
+        string with song's path
+    sf: int > 0
+        sampling frequency of song (default = 44100)
+    '''
+    samples, sf = librosa.load(song_path, sr=sf)
+    song_name = song_path[-song_path[::-1].find("/") :
+                          -song_path[::-1].find(".") - 1] if "/" in song_path
+                          else song_pathsong_path[: -song_path[::-1].find(".") - 1]
+    song_data[song_name] = (samples, sf, song_path)
